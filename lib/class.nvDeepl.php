b11760ebba5576808d241f521dd76b292bed2254
novinet-markusd <markus.dick@novinet.de>
2022-01-27 19:47:42 +0100

    update system

--- lib/class.nvDeepl.php
+++ lib/class.nvDeepl.php
@@ -13,24 +13,70 @@
 		$cid = $params['clang'];												//ID der Sprachversion
 		$ctype = $params['ctype'];												//ID der Spalte
 
+		$options = "";
+		foreach(rex_clang::getAll() as $lang) {
+			if($lang->getId() == $cid) continue;
+			$options .= '<option value="' . $lang->getId() . '">' . $lang->getName() . '</option>';
+		} 
+
+		$script = <<<JS
+			var select = document.getElementById("nv_deepl-lang-translate-select");
+			var link = document.getElementById("nv_deepl-lang-translate-link");
+
+			function nv_deepl_change_link(_) {
+				// get the current selected lang id
+				var value = select.value;
+				var href = link.getAttribute("href");
+				// deconstruct the href of the link
+				var split = href.split("&");
+				var buffer = [];
+				for(var i = 0; i < split.length; i++) {
+					var ele = split[i];
+					var inner_split = ele.split("=");
+					
+					if(inner_split[0] && inner_split[0] == "target_clang_id") {
+						// set the current selected lang id
+						inner_split[1] = value;
+					}
+
+					buffer.push(inner_split.join("="));
+				}
+
+				// rebuild the href and set it
+				link.setAttribute("href", buffer.join("&"));
+			}
+
+			if(select && link) {
+				nv_deepl_change_link();	
+				select.addEventListener("change", nv_deepl_change_link.bind(this));
+			}
+
+			if(link) {
+				link.addEventListener("click", function(event) {
+					event.preventDefault();
+					var width = window.innerWidth * 0.66 ;
+					// define the height in
+					var height = width * window.innerHeight / window.innerWidth ;
+					// Ratio the hight to the width as the user screen ratio
+					window.open(this.href , 'newwindow', 'width=' + width + ', height=' + height + ', top=' + ((window.innerHeight - height) / 2) + ', left=' + ((window.innerWidth - width) / 2));
+				});
+			}
+		
+		JS;
 
 		$panel = "";
-		$panel .= <<<EOD
-	<div class="nv_deepl">
-		<form>
-			<div class="rex-js-widget">
-				<div class="input-group">
-					<input class="form-control" type="text" name="seocu-keyword" value="$keyword" placeholder="$l1" data-seocu-aid="$aid" data-seocu-cid="$cid" />
-					<span class="input-group-btn">
-						<a class="btn btn-popup" title="$l2"><i class="rex-icon fa-refresh"></i></a>
-					</span>
+		$panel .= <<<HTML
+			<div class="nv_deepl">
+				<div class="form-group">
+					<label class="control-label" for="nv_deepl-lang-translate-select">Übersetzen in</label>
+					<select class="form-control" id="nv_deepl-lang-translate-select">
+						$options
+					</select>
 				</div>
-			</div>
-		</form>
-		<br>
-		<a href="/redaxo/index.php?page=nv_deepl/translate&article_id=$aid&ctype=$ctype&clang_id=$cid&target_clang_id=2" class="btn btn-primary pull-left" target="_blank">Übersetzen</a>
-	</div>	
-EOD;
+				<a id="nv_deepl-lang-translate-link" href="/redaxo/index.php?page=nv_deepl/translate&article_id=$aid&clang_id=$cid&target_clang_id=2" class="btn btn-primary pull-left" target="_blank">Übersetzen</a>
+				<script type="text/javascript">$script</script>
+			</div>	
+		HTML;
 
 
 		//SEO-Panel erstellen und ausgeben
@@ -49,7 +95,7 @@ EOD;
 	}
 
 
-	static function translate($sText,$sLangCode) {
+	static function translate($sText,$sLangCode,$sSourceCode="DE") {
 		$oAddon = rex_addon::get(self::$oAddon);
 
 		$sApiKey = $oAddon->getConfig("api_key");
@@ -58,7 +104,7 @@ EOD;
 			'auth_key' => $sApiKey,
 			'text' => $sText,
 			'target_lang'   => $sLangCode,
-			'source_lang' => 'DE'
+			'source_lang' => $sSourceCode
 		];
 		
 		$ch = curl_init('https://api-free.deepl.com/v2/translate');
@@ -76,5 +122,25 @@ EOD;
 
 	}
 
+	static function get_usage() {
+		$oAddon = rex_addon::get(self::$oAddon);
 
+		$sApiKey = $oAddon->getConfig("api_key");
+
+		$aParams = [
+			'auth_key' => $sApiKey,
+		];
+
+		$ch = curl_init('https://api-free.deepl.com/v2/usage');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $aParams);
+
+		// execute!
+		$oResponse = curl_exec($ch);
+	
+		// close the connection, release resources used
+		curl_close($ch);
+
+		return $oResponse;
+	}
 }
--- pages/translate.php
+++ pages/translate.php
@@ -1,6 +1,7 @@
 <?php
 
 rex_response::cleanOutputBuffers();
+$sql = rex_sql::factory();
 
 // add page param to all adminer urls
 ob_start(function ($output) {
@@ -8,113 +9,146 @@ ob_start(function ($output) {
 });
 
 $iArticleId = rex_get('article_id', 'int');
-$iCtype = rex_get('ctype', 'int');
 $iOriginLangId = rex_get('clang_id', 'int');
 $iTargetLangId = rex_get('target_clang_id', 'int');
+$target_lang_code = rex_clang::get($iTargetLangId)->getCode();
+$article = rex_article::get($iArticleId);
+
+$query = "SELECT attributes FROM rex_template WHERE id=:id LIMIT 1";
+$sql->setQuery($query, ["id" => $article->getTemplateId()]);
+$attributes = $sql->getValue("attributes");
+$attributes = json_decode($attributes, true);
+
+function removeElementsByTagName($tagName, $document) {
+  $nodeList = $document->getElementsByTagName($tagName);
+  for ($nodeIdx = $nodeList->length; --$nodeIdx >= 0; ) {
+    $node = $nodeList->item($nodeIdx);
+    $node->parentNode->removeChild($node);
+  }
+}
+
+$query = "SELECT name from rex_module WHERE id=:id LIMIT 1";
+$sql->prepareQuery($query);
 
 
-$iCtype = "-1";
 
+rex::setProperty('redaxo', false);
+$slices = rex_article_slice::getSlicesForArticle($iArticleId);
+$clean_slices_texts = [];
+$translated_slices = [];
+$i = 0;
+foreach($slices as $slice) {
+    if(!$slice->isOnline()) continue;
+    $id = $slice->getModuleId();
+    $sql->execute(["id" => $id]);
+    $doc = new DOMDocument();
+    $doc->loadHTML($slice->getSlice());
+    removeElementsByTagName('script', $doc);
+    removeElementsByTagName('style', $doc);
+    removeElementsByTagName('link', $doc);
+    $text = utf8_decode($doc->saveHTML($doc->documentElement));
+    $text = sprogdown($text, $iOriginLangId);
+    $text = strip_tags($text);
+    $text = str_replace(array("\n", "\t", "\r"), '', $text);
+    $text = trim($text);
+   
+    if($text) {
+        $ctype = $attributes["ctype"][$slice->getCtype()] ? $attributes["ctype"][$slice->getCtype()] : false;
+        $clean_slices_texts[$i] = ["ctype" => $ctype, "text" => $text, "name" => $sql->getValue("name")];
+        $oTranslated = json_decode(nvDeepl::translate($text, $target_lang_code), true);
+        $translated_slices[$i] = $oTranslated["translations"][0]["text"];
+        //$translated_slices[$i] = $text;
+        $i++;
+    }
+}
+
+rex::setProperty('redaxo', true);
+
+$usage = nvDeepl::get_usage();
+$usage = json_decode($usage, true);
+$limit = intval($usage["character_limit"]);
+$current_count = intval($usage["character_count"]);
+$remains = $limit - $current_count;
 ?>
 
 
 <!doctype html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/theme/public/assets/frontend/addons/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <title>Übersetzung - API</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/addons/be_style/plugins/redaxo/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/addons/be_style/plugins/redaxo/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/addons/be_style/plugins/redaxo/icons/favicon-16x16.png">
+        <link rel="manifest" href="/assets/addons/be_style/plugins/redaxo/icons/site.webmanifest">
+        <link rel="mask-icon" href="/assets/addons/be_style/plugins/redaxo/icons/safari-pinned-tab.svg" color="#4d99d3">
+        <meta name="msapplication-TileColor" content="#2d89ef">
+        </head>
+    <body>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>abc</title>
-</head>
-
-<body>
-
-
-
-
-    <div class="container-fluid mt-5">
-        <div class="row">
-
-            <div class="col-6">
-                <?php echo rex_clang::get($iOriginLangId)->getName(); ?>
-                <hr>
-
-
-                <?php
-                rex::setProperty('redaxo', false);
-                $oContent = new rex_article_content($iArticleId, $iOriginLangId);
-
-                $sContent = $oContent->getArticle($iCtype);
-
-                $sContent = strip_tags($sContent);
-
-                echo $sContent;
-
-                rex::setProperty('redaxo', true);
-                /*
-$oDbQ = rex_sql::factory();
-$sQuery = "SELECT s.* FROM " . rex::getTablePrefix() . "article_slice AS s LEFT JOIN " . rex::getTablePrefix() . "article AS a ON s.article_id = a.id WHERE (a.id = '$iArticleId' OR a.path LIKE '|$iArticleId|%') && a.clang_id = '" . $iOriginLangId . "'  && s.clang_id = '" . $iOriginLangId . "' ORDER BY s.priority ASC";
-$oDbQ->setQuery($sQuery);
-foreach ($oDbQ as $oRow) {
-
-    $oSlice = new rex_article_slice($oRow->getValue("id"));
-    dump($oSlice);
-
-}*/
-                ?>
-
+        <div class="container-fluid mt-5">
+            <div class="row justify-content-between align-items-start mb-5">
+                <div class="col-md-6 mb-4">
+                    <h1>Übersetzungs - API</h1>
+                </div>
+                <div class="col-md-6">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <th scope="row">Zeichen Limit</th>
+                                <td><?=$limit?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Zeichen verbraucht</th>
+                                <td><?=$current_count?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Zeichen verbleibend</th>
+                                <td><?=$remains?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-            <div class="col-6">
-                <?php echo rex_clang::get($iTargetLangId)->getName(); ?>
-                <hr>
-
-                <? $oTranslated = json_decode(nvDeepl::translate($sContent, rex_clang::get($iTargetLangId)->getCode()), true);
-                echo $oTranslated["translations"][0]["text"]; ?>
-
-
-
-
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Modul</th>
+                                <th scope="col"><?=rex_clang::get($iOriginLangId)->getName()?></th>
+                                <th scope="col"><?=rex_clang::get($iTargetLangId)->getName()?></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            for($i = 0; $i < count($clean_slices_texts); $i++) {
+                                $slice = $clean_slices_texts[$i];
+                                $text = $slice["text"];
+                                $other_text = $translated_slices[$i];
+                                $ctype = $slice["ctype"];
+                                $name = $ctype ? $slice["name"] . "<br>(" . $ctype . ")" : $slice["name"];
+                                echo <<<HTML
+                                    <tr>
+                                        <th class="col-2" scope="row">$name</th>
+                                        <td class="col-5">$text</td>
+                                        <td class="col-5"><textarea rows="5" style="background-color: transparent;border: none;min-width: 100%;resize:none;min-height:100%">$other_text</textarea></td>
+                                    </tr>
+                                HTML;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
         </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-</body>
-
+    </body>
 </html>
-
 
 <?php // make sure the output buffer callback is called
 while (ob_get_level()) {
     ob_end_flush();
 }
-
-
 exit;
